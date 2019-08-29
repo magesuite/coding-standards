ce30d08df982f91912a62d0b50bad51d24345952
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2019-08-29 13:19:46 +0200

    Updated MethodArguments sniff for plugin methods

--- MageSuite/Sniffs/TooMany/MethodArgumentsSniff.php
+++ MageSuite/Sniffs/TooMany/MethodArgumentsSniff.php
@@ -12,6 +12,8 @@ class MethodArgumentsSniff implements \PHP_CodeSniffer\Sniffs\Sniff
 
     public $argumentsLimit = 3;
 
+    protected $pluginPrefixes = ['before', 'after', 'around'];
+
     public function register()
     {
         return [T_FUNCTION];
@@ -36,10 +38,30 @@ class MethodArgumentsSniff implements \PHP_CodeSniffer\Sniffs\Sniff
         }
 
         if ($parametersCount > $this->argumentsLimit) {
+
+            if ($this->isPluginMethod($phpcsFile->getFilename(), $methodName)) {
+                return;
+            }
+
             $error = 'Too many parameters in %s() method (%s found, %s max)';
             $data = [$methodName, $parametersCount, $this->argumentsLimit];
 
             $phpcsFile->addWarning($error, $position, 'Found', $data);
         }
+    }
+
+    private function isPluginMethod($filePath, $methodName)
+    {
+        if (strpos($filePath, '/Plugin/') === false) {
+            return false;
+        }
+
+        foreach ($this->pluginPrefixes as $prefix) {
+            if (strpos($methodName, $prefix) === 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
